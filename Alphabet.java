abf8bf477dcb9b9a2bc54893f4f6d1a34a6a86d2
Swapy78 <swapnilbabanmane@gmail.com>
2023-04-12 20:41:27 +0530

    Added Java Program to Check Whether an Alphabet is Vowel or Consonant

---
+++ Alphabet.java
@@ -0,0 +1,28 @@
+//Java Program to Check Whether an Alphabet is Vowel or Consonant
+
+package practice;
+
+
+
+public class Alphabet {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch = 'z';
+	
+		
+		
+		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+				System.out.println(ch + " Alphabet is vowel"); 
+	
+
+	else 
+		System.out.println(ch + " Alphabet is consonant");
+	
+
+
+			
+ 
+}
+}
