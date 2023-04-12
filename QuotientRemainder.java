76ce3dfd902ef02d1dd4287eefed8db62e21ecde
Swapy78 <swapnilbabanmane@gmail.com>
2023-04-12 20:37:45 +0530

    Added Java Program to Compute Quotient and Remainder

---
+++ QuotientRemainder.java
@@ -0,0 +1,19 @@
+//Java Program to Compute Quotient and Remainder
+
+package practice;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dividend = 547;
+		int divisor = 23;
+		
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+		
+		System.out.println("Quotient is "+quotient);
+		System.out.println("Remainder is "+remainder);
+	}
+
+}
