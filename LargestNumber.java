129ad22f6225c109c9d331ce9c3b2c6fdcf81add
Swapy78 <swapnilbabanmane@gmail.com>
2023-04-12 20:42:45 +0530

    Added Java Program to Find the Largest Among Three Numbers

---
+++ LargestNumber.java
@@ -0,0 +1,26 @@
+// Java Program to Find the Largest Among Three Numbers
+
+package practice;
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 123; 
+		int b = 547;
+		int c = 223;
+		
+		if(a > b && a > c) {
+			System.out.println("Largest number is "+a);
+		}
+			if(b > a && b > c ){
+				System.out.println("Largest number is "+b);
+			}
+				if(c > a && c > b){
+					System.out.println("Largest number is "+c);
+				}
+				}
+			}
+		
+		
+		
