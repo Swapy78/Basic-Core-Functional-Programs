c2e1dcf3165a60305884386028403e938a9e9f34
Swapy78 <swapnilbabanmane@gmail.com>
2023-04-12 20:40:13 +0530

    Added Java Program to Check Whether a Number is Even or Odd

---
+++ EvenOdd.java
@@ -0,0 +1,21 @@
+//Java Program to Check Whether a Number is Even or Odd
+
+package practice;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 27;
+		
+		if(number % 2 == 0) {
+			System.out.println("Number is even");
+			
+		}
+		else {
+			System.out.println("Number is odd");
+		}
+
+	}
+
+}
