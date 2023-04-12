5a48ccbed9351d8163f4082705d003257b400e47
Swapy78 <swapnilbabanmane@gmail.com>
2023-04-12 20:30:18 +0530

    Added Java program to check leap year

---
+++ LeapYear.java
@@ -0,0 +1,19 @@
+//Java program to check leap year
+
+package practice;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year = 1995;
+		
+		if(year % 4 == 0) {
+			System.out.println("1995 is the leap year");
+		}
+		else {
+			System.out.println("1995 is not the leap year");
+		}
+	}
+
+}
